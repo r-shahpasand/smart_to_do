6d4571e8011fb95dff217a4edc59cb07d1ce9895
Shahpasand <ra.shahpasand@gmail.com>
2025-04-10 13:13:25 +0430

    hive as local data source

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,11 +1,26 @@
 import 'package:smart_to_do/models/priority.dart';
+import 'package:hive/hive.dart';
 
-class Task {
+part 'task.g.dart';
+
+@HiveType(typeId: 0) // use Hive to generate a type adapter
+class Task extends HiveObject{
+  @HiveField(0)
   final int id;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final String? description;
+
+  @HiveField(3)
   final bool isCompleted;
+
+  @HiveField(4)
   final DateTime dueTime;
+
+  @HiveField(5)
   final Priority priority;
 
   Task({
